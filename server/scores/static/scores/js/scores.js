bff1c2afeb8df9830e74edb25895d29f7fefb01a
grintroy <grintroy@gmail.com>
2024-03-22 20:24:31 +0000

    Add support for custom hosts

--- server/scores/static/scores/js/scores.js
+++ server/scores/static/scores/js/scores.js
@@ -1,34 +1,4 @@
-const HOST = "127.0.0.1:8000";
-
-const socket = new WebSocket("ws://" + HOST + "/ws/scores/");
-
-socket.onopen = (e) => {
-	console.log("Socket successfully connected.");
-};
-
-socket.onclose = (e) => {
-	console.log("Chat socket closed unexpectedly");
-};
-
-socket.onmessage = (e) => {
-	const data = JSON.parse(e.data);
-	console.log(data);
-	switch (data.event) {
-		case "update_scores":
-			const scores = data.scores;
-			for (let s of scores) {
-				const color = s["color"];
-				const score = s["score"];
-				const score_element = document.getElementById(`score-${color}`);
-				score_element.textContent = score;
-			}
-			break;
-		case "update_transform":
-			break;
-		default:
-			console.log("Unknown event for incoming message.");
-	}
-};
+let socket;
 
 function add_points(color, n) {
 	socket.send(JSON.stringify({ event: "add_points", color: color, n: n }));
@@ -69,4 +39,68 @@ document.querySelectorAll(".form-points").forEach((form) => {
 		const points = form_data.get("points");
 		set_points(color, points);
 	});
+});
+
+document.getElementById("form-connect").addEventListener("submit", (e) => {
+	e.preventDefault();
+	const form_data = new FormData(document.getElementById("form-connect"));
+	const host = form_data.get("host");
+
+	socket = new WebSocket("ws://" + host + "/ws/scores/");
+
+	socket.onopen = (e) => {
+		const button = document.getElementById("form-connect-button");
+		button.classList.remove("btn-primary");
+		button.classList.add("btn-success");
+		button.textContent = "Connected";
+		button.disabled = true;
+		const input = document.getElementById("form-connect-input");
+		input.disabled = true;
+
+		const container = document.getElementById("controls");
+		container.querySelectorAll("input, button").forEach((input) => {
+			input.disabled = false;
+		});
+
+		console.log("Socket successfully connected.");
+	};
+
+	socket.onclose = (e) => {
+		console.log("Chat socket closed unexpectedly");
+	};
+
+	socket.onmessage = (e) => {
+		const data = JSON.parse(e.data);
+		console.log(data);
+		switch (data.event) {
+			case "update_scores":
+				const scores = data.scores;
+				for (let s of scores) {
+					const color = s["color"];
+					const score = s["score"];
+					const score_element = document.getElementById(
+						`score-${color}`
+					);
+					score_element.textContent = score;
+				}
+				break;
+			case "update_transform":
+				break;
+			default:
+				console.log("Unknown event for incoming message.");
+		}
+	};
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	host_field = document.getElementById("form-connect-input");
+	if (host_field.value) {
+		const button = document.getElementById("form-connect-button");
+		button.click();
+	} else {
+		const container = document.getElementById("controls");
+		container.querySelectorAll("input, button").forEach((input) => {
+			input.disabled = true;
+		});
+	}
 });
--- server/scores/views.py
+++ server/scores/views.py
@@ -8,11 +8,14 @@ def index(request):
 
 
 def scores(request):
+    if request.method == "GET":
+        host = request.GET.get("host")
     return render(
         request,
         "scores.html",
         {
             "title": "Scores",
             "teams": Team.objects.all().order_by("index"),
+            "host": host,
         },
     )
