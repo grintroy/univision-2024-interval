79c1085f6c852802b3f6c60c60a70d3e6a8a4da0
grintroy <grintroy@gmail.com>
2024-03-22 22:17:47 +0000

    Add motion blur to bigtitle

--- template/assets/js/bigtitle.js
+++ template/assets/js/bigtitle.js
@@ -1,20 +1,36 @@
+const MOTION_BLUR_DELAY = 4; // ms
+
 webcg.on("data", (data) => {
-	const title = document.querySelector("#bigtitle");
-	title.innerHTML = data.title;
+	const title = document.querySelectorAll(".title");
+	title.forEach((element) => {
+		element.innerHTML = data.title;
+	});
 });
 
 webcg.on("play", () => {
-	const title = document.querySelector("#bigtitle");
-	title.classList.add("in");
-	title.classList.remove("init", "out");
+	const title = document.querySelectorAll(".title");
+	let delay = 0;
+	title.forEach((element) => {
+		setTimeout(() => {
+			element.classList.add("in");
+			element.classList.remove("init", "out");
+		}, delay);
+		delay += MOTION_BLUR_DELAY;
+	});
 });
 
 webcg.on("stop", () => {
-	const title = document.querySelector("#bigtitle");
-	title.classList.add("out");
-	title.classList.remove("in");
-	setTimeout(() => {
-		title.classList.add("init");
-		title.classList.remove("out");
-	}, 700);
+	const title = document.querySelectorAll(".title");
+	let delay = 0;
+	title.forEach((element) => {
+		setTimeout(() => {
+			element.classList.add("out");
+			element.classList.remove("in");
+		}, delay);
+		setTimeout(() => {
+			element.classList.add("init");
+			element.classList.remove("out");
+		}, 1000);
+		delay += MOTION_BLUR_DELAY;
+	});
 });
